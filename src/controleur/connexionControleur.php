710080ce4a188e900238c037cf9f7846229fa571
Tom <rizzitom52@gmail.com>
2024-12-09 15:59:32 +0100

    connexion qui marche

--- src/controleur/connexionControleur.php
+++ src/controleur/connexionControleur.php
@@ -1,26 +1,31 @@
 <?php
+require_once '../src/modele/class_utilisateur.php';
+
 function connexionControleur($twig, $db){
     $form = array();
+
     if (isset($_POST['btConnecter'])){
-        $form['valide'] = true;
-        $inputEmail = $_POST['inputEmail'];
-        $inputPassword = $_POST['inputPassword'];
-        $utilisateur = new Utilisateur($db);
-        $unUtilisateur = $utilisateur->connect($inputEmail);
-        if ($unUtilisateur!=null){
-            if(!password_verify($inputPassword,$unUtilisateur['mdp'])){
-                $form['valide'] = false;
-                $form['message'] = 'Login ou mot de passe incorrect';
-            }
-            
-            else{
-                header("Location:index.php");
-                exit;
-            }
-        }
-        else{
+      $form['valide'] = true;
+      $inputEmail = filter_var($_POST['inputEmail'], FILTER_SANITIZE_EMAIL);
+      $inputPassword = trim($_POST['inputPassword']);
+      $utilisateur = new Utilisateur($db);
+      $unUtilisateur = $utilisateur->connect($inputEmail);
+      if ($unUtilisateur!=null){
+        if(!password_verify($inputPassword,$unUtilisateur['mdp'])){
             $form['valide'] = false;
             $form['message'] = 'Login ou mot de passe incorrect';
+          }
+          else{
+            session_start();
+            $_SESSION['email'] = $unUtilisateur['email']; 
+            $_SESSION['role'] = $unUtilisateur['role'];
+            header("Location:index.php");
+            exit;
+          }
+        }
+        else{
+        $form['valide'] = false;
+        $form['message'] = 'Login ou mot de passe incorrect';
         }
     }
     echo $twig->render('connexion.html.twig', array('form'=>$form));
